5cfc29ea64be6b536384922da1667c770d1bd179
Pave010 <131611916+Pave010@users.noreply.github.com>
2023-04-23 11:59:42 -0600

    Add files via upload

---
+++ Form calculadora.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtNum1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            double Num1 = double.Parse(txtNum1.Text);
+            double Num2 = double.Parse(txtNum2.Text);
+
+            double res = Num1 + Num2;
+            txtResultado.Text = res.ToString();
+        }
+
+        private void txtResultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            double Num1 = double.Parse(txtNum1.Text);
+            double Num2 = double.Parse(txtNum2.Text);
+
+            double res = Num1 / Num2;
+            if(Num2 == 0)
+            {
+                MessageBox.Show("No es posible realizar la division", "ERROR");
+            }
+            else { txtResultado.Text = res.ToString(); }
+           
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            double Num1 = double.Parse(txtNum1.Text);
+            double Num2 = double.Parse(txtNum2.Text);
+
+            double res = Num1 - Num2;
+            txtResultado.Text = res.ToString();
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            double Num1 = double.Parse(txtNum1.Text);
+            double Num2 = double.Parse(txtNum2.Text);
+
+            double res = Num1 * Num2;
+            txtResultado.Text = res.ToString();
+        }
+    }
+}
